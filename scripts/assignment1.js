7c1dfcb92cbca2571fe685bc17631708327bfef0
RamshaSultan <ramsha.sultan101@gmail.com>
2023-04-16 01:47:27 +0500

    FEAT: Initialzie Project

---
+++ scripts/assignment1.js
@@ -0,0 +1,50 @@
+// TASK NO.1
+// Calculate the zakat value, first, create a variable named "zakatPercentage" and
+// store the value of 2.5% in it (hint: 2.5% means 0.025). Next, create another variable
+// named "userInput" and take the input from the user using the prompt. Make sure
+// the input value is of a type number by converting the input string to a number
+// using a suitable method. Then, create a variable named "result" and assign its value
+// to the multiplication of the zakat percentage and user input. Finally, use an alert
+// message to display the calculated zakat value. The message should look like this:
+// "Your zakat value is xxx".
+
+// ----------------------------------------------------------------------------------------------------------
+
+// let ZakatPercentage = 0.025;
+// let UserInput = prompt("Enter Your Zakat Amount");
+// let Result = ZakatPercentage * (UserInput);
+// alert("Your Zakat is " + Result)
+
+
+
+// TASK NO. 2
+
+// calculate the fitrah amount, first, ask the user to enter the total number of family
+// members using the prompt and store the value in a variable called
+// "familyMembers". Next, ask the user to choose a fitrah method by providing them
+// options using the prompt, and store the selected method and its price in variables.
+// Then, use an if-else block to check the user's input and calculate the fitrah amount
+// by multiplying the selected method's price with the number of family members.
+// Finally, display the calculated fitrah amount using an alert message.
+
+// -------------------------------------------------------------------------------------------------------------
+
+
+// let FamilyMembers = prompt("Please Enter the Number of family Member")
+
+// let FitraMetod = prompt("Please Select Fitra Method (1)Wheat 250Rs, (2)Barly 450Rs, (3)Dates 2100Rs, (4)Raisin 2800Rs")
+
+// if (FitraMetod === "1") {
+//     console.log("Your Fitra Amount is ", 250 * FamilyMembers)
+// }else if (FitraMetod === "2") {
+//     console.log ("Your Fitra Amount is ", 450 * FamilyMembers)
+// }else if (FitraMetod === "3") {
+//     console.log ("Your Fitra Amount is ", 2100 * FamilyMembers)
+// }else if (FitraMetod === "4") {
+//     console.log ("Your Fitra Amount is ", 2800 * FamilyMembers)
+// }
+//  else {
+//     console.log("Invalid Selection, Please Try Again")
+// }
+
+ 
